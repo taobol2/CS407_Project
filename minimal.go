ec6c29d59211870fd9323548bd20f5be646213ba
taobol2 <166887415+taobol2@users.noreply.github.com>
2024-12-18 18:43:11 -0600

    poly benchmark

--- minimal.go
+++ minimal.go
@@ -11,58 +11,61 @@ import (
 	"github.com/consensys/gnark/frontend/cs/r1cs"
 )
 
-type EvaluateAtOneCircuit struct {
-	A []frontend.Variable `gnark:"a,public"`
-	S []frontend.Variable `gnark:"s,public"`
-	B []frontend.Variable `gnark:"b,public"`
-	T []frontend.Variable `gnark:"t,public"`
+// EvaluateBezoutCircuit checks that (a(x)*s(x) + b(x)*t(x)) = 1 for given polynomials a,s,b,t and a witness x.
+type EvaluateBezoutCircuit struct {
+	A []frontend.Variable `gnark:"a,public"` // coefficients of A(x)
+	S []frontend.Variable `gnark:"s,public"` // coefficients of S(x)
+	B []frontend.Variable `gnark:"b,public"` // coefficients of B(x)
+	T []frontend.Variable `gnark:"t,public"` // coefficients of T(x)
+	X frontend.Variable   `gnark:"x"`        // witness where we evaluate the polynomials
 }
 
-func (c *EvaluateAtOneCircuit) Define(api frontend.API) error {
-	nA := len(c.A)
-	nB := len(c.B)
-
-	// Initialize sums with the first coefficient
-	aSum := c.A[0]
-	for i := 1; i < nA; i++ {
-		aSum = api.Add(aSum, c.A[i])
+func (c *EvaluateBezoutCircuit) Define(api frontend.API) error {
+	// Evaluate a(x)
+	aVal := frontend.Variable(0)
+	xPow := frontend.Variable(1)
+	for i := 0; i < len(c.A); i++ {
+		aVal = api.Add(aVal, api.Mul(c.A[i], xPow))
+		xPow = api.Mul(xPow, c.X)
 	}
 
-	sSum := c.S[0]
-	for i := 1; i < nA; i++ {
-		sSum = api.Add(sSum, c.S[i])
+	// Evaluate s(x)
+	sVal := frontend.Variable(0)
+	xPow = frontend.Variable(1)
+	for i := 0; i < len(c.S); i++ {
+		sVal = api.Add(sVal, api.Mul(c.S[i], xPow))
+		xPow = api.Mul(xPow, c.X)
 	}
 
-	bSum := c.B[0]
-	for i := 1; i < nB; i++ {
-		bSum = api.Add(bSum, c.B[i])
+	// Evaluate b(x)
+	bVal := frontend.Variable(0)
+	xPow = frontend.Variable(1)
+	for i := 0; i < len(c.B); i++ {
+		bVal = api.Add(bVal, api.Mul(c.B[i], xPow))
+		xPow = api.Mul(xPow, c.X)
 	}
 
-	tSum := c.T[0]
-	for i := 1; i < nB; i++ {
-		tSum = api.Add(tSum, c.T[i])
+	// Evaluate t(x)
+	tVal := frontend.Variable(0)
+	xPow = frontend.Variable(1)
+	for i := 0; i < len(c.T); i++ {
+		tVal = api.Add(tVal, api.Mul(c.T[i], xPow))
+		xPow = api.Mul(xPow, c.X)
 	}
 
-	// (aSum*sSum) + (bSum*tSum)
-	lhs := api.Add(api.Mul(aSum, sSum), api.Mul(bSum, tSum))
+	// Compute a(x)*s(x) + b(x)*t(x)
+	lhs := api.Add(api.Mul(aVal, sVal), api.Mul(bVal, tVal))
 
-	// lhs must equal 1
+	// Assert (a(x)*s(x) + b(x)*t(x)) = 1
 	api.AssertIsEqual(lhs, 1)
 
 	return nil
 }
 
-func NextPowerOfTwo(n int) int {
-	p := 1
-	for p < n {
-		p <<= 1
-	}
-	return p
-}
-
 func main() {
 	rand.Seed(time.Now().UnixNano())
 
+	// Example degrees:
 	degAs := []int{100000, 200000, 300000, 400000, 500000, 600000}
 	degBs := []int{100, 200, 400, 800, 1000}
 
@@ -76,6 +79,7 @@ func main() {
 			lenB := degB + 1
 			lenT := lenB
 
+			// Generate random coefficients for A,S,B,T
 			A := make([]frontend.Variable, lenA)
 			S := make([]frontend.Variable, lenS)
 			B := make([]frontend.Variable, lenB)
@@ -90,34 +94,32 @@ func main() {
 				T[i] = rand.Int63()
 			}
 
-			var circuit EvaluateAtOneCircuit
-			circuit.A = make([]frontend.Variable, lenA)
-			circuit.S = make([]frontend.Variable, lenS)
-			circuit.B = make([]frontend.Variable, lenB)
-			circuit.T = make([]frontend.Variable, lenT)
-			copy(circuit.A, A)
-			copy(circuit.S, S)
-			copy(circuit.B, B)
-			copy(circuit.T, T)
+			// Generate a random witness x
+			x := rand.Int63()
+
+			var circuit EvaluateBezoutCircuit
+			circuit.A = append([]frontend.Variable(nil), A...)
+			circuit.S = append([]frontend.Variable(nil), S...)
+			circuit.B = append([]frontend.Variable(nil), B...)
+			circuit.T = append([]frontend.Variable(nil), T...)
+			circuit.X = x
 
 			startCompile := time.Now()
-			ccs, err := frontend.Compile(fr.Modulus(), r1cs.NewBuilder, &circuit)
+			_, err := frontend.Compile(fr.Modulus(), r1cs.NewBuilder, &circuit)
 			if err != nil {
 				log.Fatal("circuit compilation failed:", err)
 			}
 			timeCompile := time.Since(startCompile).Milliseconds()
 
-			// Use ccs to avoid declared and not used error
-			nbConstraints := ccs.GetNbConstraints()
-			_ = nbConstraints // We won't print it to keep output clean, but now ccs is used.
-
 			startWitness := time.Now()
-			witness := EvaluateAtOneCircuit{
+			witness := EvaluateBezoutCircuit{
 				A: A,
 				S: S,
 				B: B,
 				T: T,
+				X: x,
 			}
+
 			_, err = frontend.NewWitness(&witness, fr.Modulus())
 			if err != nil {
 				log.Fatal("Failed to create witness:", err)
